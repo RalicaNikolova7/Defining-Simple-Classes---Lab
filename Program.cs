b7fd51a3cb4716926f9bb83516c7cabf21f2a623
RalicaNikolova7 <127286692+RalicaNikolova7@users.noreply.github.com>
2024-10-16 08:38:34 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Drawing;
+
+namespace RectangleClass;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Rectangle rectangle = new Rectangle();
+
+        rectangle.Width = int.Parse(Console.ReadLine());
+        rectangle.Height = int.Parse(Console.ReadLine());
+        rectangle.Color = Console.ReadLine();
+
+        Console.WriteLine($"Rect({rectangle.Width}, {rectangle.Height}, {rectangle.Color}) has area {rectangle.CalculateArea()}.");
+    }
+}
+
+public class Rectangle
+{
+    public int Width { get; set; }
+    public int Height { get; set; }
+    public string Color { get; set; }
+
+    public int CalculateArea()
+    {
+        return Width * Height;
+    }
+}
