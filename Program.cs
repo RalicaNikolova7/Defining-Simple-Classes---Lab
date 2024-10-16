399146ff7bbead33710102a132aa2d50f12e2585
RalicaNikolova7 <127286692+RalicaNikolova7@users.noreply.github.com>
2024-10-16 08:41:43 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,28 +1,20 @@
-﻿namespace RandomizeWords;
+﻿using System.Numerics;
+namespace BigFactorial;
 
 class Program
 {
     static void Main(string[] args)
     {
-        string[] words = Console.ReadLine().Split(' ');
+        int n = int.Parse(Console.ReadLine());
 
-        Random rnd = new Random();
+        BigInteger f = 1;
 
-        for (int pos1 = 0; pos1 < words.Length; pos1++)
+        for (int i = 2; i <= n; i++)
         {
-
-            int pos2 = rnd.Next(words.Length);
-
-            var a = words[pos2];
-            var b = words[pos1];
-
-            words[pos2] = b;
-            words[pos1] = a;
-
-            // TODO: Swap words[pos1] with words[pos2]
+            f *= i;
         }
 
-        Console.WriteLine(string.Join(Environment.NewLine, words));
+        Console.WriteLine(f);
     }
 }
 
